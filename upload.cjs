919d98753f7ce3c88f6c3cd6baaebb92e2f9e924
Paul Zoltán <paulzoltan02@gmail.com>
2024-05-14 11:26:09 +0200

    initial commit

--- upload.cjs
+++ upload.cjs
@@ -3,14 +3,14 @@
 const ftp = require('basic-ftp')
 const glob = require('glob')
 const { execSync } = require('child_process')
+const minimist = require('minimist')
 
 async function getCurrentBranch() {
   try {
-    // Execute the 'git branch --show-current' command to get the current branch name
     return execSync('git branch --show-current').toString().trim()
   } catch (err) {
     console.error('Error getting current branch:', err)
-    process.exit(1) // Exit the script with an error code
+    process.exit(1)
   }
 }
 
@@ -20,33 +20,29 @@ const credentials = {
   password: process.env.JNKR_FTP_PASS,
 }
 
-async function uploadFile(key, modifier) {
-  if (modifier !== '--ignore-branch') {
+async function uploadFile(key, ignoreBranch, basePath) {
+  if (!ignoreBranch) {
     const currentBranch = await getCurrentBranch()
     if (key !== currentBranch) {
       console.error(
         `Error: The provided key "${key}" does not match the current branch "${currentBranch}".`
       )
-      process.exit(1) // Exit the script with an error code
+      process.exit(1)
     }
   }
 
   const client = new ftp.Client()
   try {
-    // Connect to the FTP server
     await client.access(credentials)
 
-    // Use the provided parameter in the remote filenames
-    const remotePathJS = `public/assets/${key}/index-${key}.js`
-    const remotePathCSS = `public/assets/${key}/index-${key}.css`
+    const remotePathJS = `${basePath}/${key}/index-${key}.js`
+    const remotePathCSS = `${basePath}/${key}/index-${key}.css`
 
-    // Define an array of local file patterns to match
     const localFilePatterns = [
       'dist/assets/index*.css',
       'dist/assets/index*.js',
     ]
 
-    // Find local files that match each pattern
     const localFiles = []
     for (const pattern of localFilePatterns) {
       const matchingFiles = glob.sync(pattern)
@@ -59,7 +55,6 @@ async function uploadFile(key, modifier) {
         localFilePatterns.join(', ')
       )
     } else {
-      // Upload each matching file
       for (const localFilePath of localFiles) {
         if (localFilePath.endsWith('.js')) {
           await client.uploadFrom(localFilePath, remotePathJS)
@@ -76,13 +71,39 @@ async function uploadFile(key, modifier) {
   } catch (err) {
     console.error('Error:', err)
   } finally {
-    // Close the FTP connection
     client.close()
   }
 }
 
-if (process.argv.length <= 3) {
-  console.error('Usage: node upload.js <branch-key>')
+const args = minimist(process.argv.slice(2), {
+  default: {
+    'base-path': 'public/assets',
+    'ignore-branch': false,
+  },
+  alias: {
+    'base-path': 'b',
+    'ignore-branch': 'i',
+  },
+})
+
+if (args.help || args.h) {
+  console.log(`
+    Usage: node upload.js <branch-key> [options]
+
+    Options:
+      --ignore-branch, -i  Ignore the branch check
+      --base-path=<path>, -b  Set the base path (default: 'public/assets')
+      --help, -h  Show this help message
+  `)
+  process.exit(0)
 }
-const key = process.argv[2]
-uploadFile(key, process.argv[3])
+
+if (!args._[0]) {
+  console.error(
+    'Usage: node upload.js <branch-key> [--ignore-branch] [--base-path=<base-path>]'
+  )
+  process.exit(1)
+}
+
+const key = args._[0]
+uploadFile(key, args['ignore-branch'], args['base-path'])
